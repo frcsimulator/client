ec201eb51898a0d2e7bf9e293ccf8bbb897053c5
Benjamin Pylko <brpylko@gmail.com>
2012-05-16 18:46:47 +0000

    Added arguments class, not done
    git-svn-id: https://svn.code.sf.net/p/frcsimulator/client/client/trunk@81 a72c890d-11d8-456d-a420-caba5f219908

---
+++ net/sourceforge/frcsimulator/Arguments.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sourceforge.frcsimulator;
+
+/**
+ *
+ * @author bpylko2015
+ */
+public class Arguments {
+    public Arguments(String[] args){
+        
+    }
+}
