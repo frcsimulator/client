eb31e5932f20bc1f3be99b65bba6408a71ce00d0
Benjamin Pylko <brpylko@gmail.com>
2012-05-26 19:51:27 +0000

    Got Command line arguments working with each flag taking 1 or 0 parameters, should work with more, but I am not sure
    git-svn-id: https://svn.code.sf.net/p/frcsimulator/client/client/trunk@85 a72c890d-11d8-456d-a420-caba5f219908

--- net/sourceforge/frcsimulator/Arguments.java
+++ net/sourceforge/frcsimulator/Arguments.java
@@ -4,73 +4,35 @@
  */
 package net.sourceforge.frcsimulator;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  *
  * @author bpylko2015
  */
 public class Arguments {
-    /*private String[] configStringArray;
-    private Properties args;
-    private Comparator<String> argTypeComparator = new Comparator<String>(){
 
-        @Override
-        public int compare(String o1, String o2) {
-            return (o2.contains("#") ? 0 : 1) - (o1.contains("#") ? 0 : 1);
-        }
-        
-    };
-    // configString format: @argname#numparams
-    // @0,@1 etc are unnamed arguments (eg a filename passed to the program)
-    // order does not matter
-    // eg @--gui#0 @0 is the same as @0 @--gui#0
-    public Arguments(String[] args, String configString){
-        this.args = new Properties();
-        configStringArray = configString.split(" ");
-        Arrays.sort(configStringArray,argTypeComparator);
-        String tempName = "";
-        String tempParamList = "";
-        int tempLengthDifference = 0;
-        if(args.length == 0){return;}
-        for(int i = 0; i < configStringArray.length;i++){
-            if(!configStringArray[i].contains("#") && configStringArray[i].contains("@")){
-                this.args.put(configStringArray[i].substring(1),args[i]);
-            } else{
-                tempName = configStringArray[i].split("\\#")[0].substring(1);
-                if(Integer.parseInt(configStringArray[i].substring(configStringArray[i].indexOf("#")+1)) == 0){this.args.put(tempName, Arrays.binarySearch(args, tempName) < 1 ? 0 : 1);}
-                else{
-                    tempLengthDifference = args.length-Arrays.binarySearch(args,tempName);
-                    for(int j = 0; j < configStringArray.length;j++){
-                        tempLengthDifference = Arrays.binarySearch(args, configStringArray[j].split("\\#")[0].substring(1)) - Arrays.binarySearch(args,tempName) < tempLengthDifference &&Arrays.binarySearch(args, configStringArray[j].split("\\#")[0].substring(1)) - Arrays.binarySearch(args,tempName) > 0 ? Arrays.binarySearch(args, configStringArray[j].split("\\#")[0].substring(1)) - Arrays.binarySearch(args,tempName) : 0;
-                    }
-                    for(int j = Arrays.binarySearch(args,tempName); j < Arrays.binarySearch(args,tempName)+tempLengthDifference; j++){
-                        tempParamList += args[j] + " ";
-                    }
-                    tempParamList.trim();
-                    this.args.put(tempName,tempParamList);
+    protected Properties argProperties = new Properties();
+
+    public Arguments(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                ArrayList<String> temp = new ArrayList();
+                String argName = args[i].substring(1);
+                if (argName.startsWith("-")) {
+                    argName = argName.substring(1);
                 }
+                i++;
+                for (; i < args.length && !args[i].startsWith("-"); i++) {
+                    temp.add(args[i]);
+                }
+                i--;
+                argProperties.put(argName, temp.toArray(new String[temp.size()]));
             }
         }
     }
-    public String get(String key){
-        return args.getProperty(key) == null ? null : args.get(key).toString();
+
+    public String[] get(String arg) {
+        return (String[]) argProperties.get(arg);
     }
-    */
-    String argv[];
-    public Arguments(String argv[], String configString){
-        this.argv = argv;
-    }
-    /**
-     * 
-     * @param key the argument to search for
-     * @param numArgs the number of
-     * @return The Array of parameters for the argument
-     */
-    //public String[] get(String argument, int numParams){
-    //    Arrays.search
-    //}
 }
--- net/sourceforge/frcsimulator/Client.java
+++ net/sourceforge/frcsimulator/Client.java
@@ -18,91 +18,81 @@ import net.sourceforge.frcsimulator.mistware.Simulator;
  * @author wolf
  */
 public class Client {
-	public static final int E_NONE = 0, E_BADARGS = 2, E_SIMFAIL = 5;
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		String testCase = "net.sourceforge.frcsimulator.test.FRCBotRobotBase";
-		boolean gui = false;
-		// Process arguments
-		/*if (args.length > 0) {
-			if (args[0].startsWith("-")) { // It's a flag
-				if ("--gui".equals(args[0])) {
-					gui=true;
-				} else {
-					System.err.println("Invalid flag: "+args[0]);
-					System.exit(E_BADARGS); // Bad arguments
-				}
-			} else { // It's a class
-				testCase = args[0];
-			}
-		}
-		if (args.length == 2) {
-			testCase = args[1];
-		} else if (args.length > 2) {
-			System.err.println("Too many arguments!");
-			System.exit(E_BADARGS); // Bad arguments
-		}*/
+
+    public static final int E_NONE = 0, E_BADARGS = 2, E_SIMFAIL = 5;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Arguments arguments = new Arguments(args);
+        // Process arguments
+        boolean gui = arguments.get("gui") != null || arguments.get("g") != null;
+        String testCase = arguments.get("class") != null ? arguments.get("class")[0] : (arguments.get("c") != null ? arguments.get("c")[0] : "net.sourceforge.frcsimulator.test.FRCBotRobotBase");
         try {
             CRIO.getInstance().addModule(new CRIOModule(0x01), 1);
-        } catch (ModuleException ex) {ex.printStackTrace();}
-		if (gui) {
-			simulatorGui(testCase);
-		} else {
-			simulatorCli(testCase);
-		}
-	}
-	public static void simulatorCli(String testCase) {
-		Simulator simulator;
-		try {
-			simulator = new Simulator(testCase);
-		} catch (ClassNotFoundException ex) {
-			System.err.println("The simulator couldn't find the given class!");
-			ex.printStackTrace();
-			System.exit(E_BADARGS); // Bad arguments
-			return; // Needed so NetBeans won't complain about simulator being unitinitalized.
-		}
-		simulator.getLogger().addHandler(new StreamHandler(System.out,new SimpleFormatter()));
-		try {
-			simulator.onStatusChange(Client.class.getMethod("simStateChangeCli", Simulator.Status.class, Simulator.Status.class));
-		} catch (Exception e) {
-			System.err.println("Oops, couldn't add a status change hook to the simulator.");
-			e.printStackTrace();
-		}
-		simulator.start();
-		try {
-			simulator.join();
-		} catch (InterruptedException e) {
-			System.err.println("Interrupted while join()ing simulator:");
-			e.printStackTrace();
-		}
-		if (simulator.getStatus()==Simulator.Status.ERROR) {
-			System.exit(E_SIMFAIL); // Abnormal simulator failure
-		}
-		System.exit(E_NONE);
-	}
-	public static void simStateChangeCli(Simulator.Status status, Simulator.Status oldStatus) {
-		System.out.println("Simulator status: "+status.toString());
-	}
+        } catch (ModuleException ex) {
+            ex.printStackTrace();
+        }
+        if (gui) {
+            simulatorGui(testCase);
+        } else {
+            simulatorCli(testCase);
+        }
+    }
 
-	private static void simulatorGui(final String testCase) {
-		final Object lock = new Object();
-		synchronized(lock) {
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					SimulatorControlFrame simulatorControlFrame = new SimulatorControlFrame(testCase);
-					synchronized (lock) {
-						lock.notify();
-					}
-				}
-			});
-			try {
+    public static void simulatorCli(String testCase) {
+        Simulator simulator;
+        try {
+            simulator = new Simulator(testCase);
+        } catch (ClassNotFoundException ex) {
+            System.err.println("The simulator couldn't find the given class!");
+            ex.printStackTrace();
+            System.exit(E_BADARGS); // Bad arguments
+            return; // Needed so NetBeans won't complain about simulator being unitinitalized.
+        }
+        simulator.getLogger().addHandler(new StreamHandler(System.out, new SimpleFormatter()));
+        try {
+            simulator.onStatusChange(Client.class.getMethod("simStateChangeCli", Simulator.Status.class, Simulator.Status.class));
+        } catch (Exception e) {
+            System.err.println("Oops, couldn't add a status change hook to the simulator.");
+            e.printStackTrace();
+        }
+        simulator.start();
+        try {
+            simulator.join();
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted while join()ing simulator:");
+            e.printStackTrace();
+        }
+        if (simulator.getStatus() == Simulator.Status.ERROR) {
+            System.exit(E_SIMFAIL); // Abnormal simulator failure
+        }
+        System.exit(E_NONE);
+    }
+
+    public static void simStateChangeCli(Simulator.Status status, Simulator.Status oldStatus) {
+        System.out.println("Simulator status: " + status.toString());
+    }
+
+    private static void simulatorGui(final String testCase) {
+        final Object lock = new Object();
+        synchronized (lock) {
+            SwingUtilities.invokeLater(new Runnable() {
+
+                @Override
+                public void run() {
+                    SimulatorControlFrame simulatorControlFrame = new SimulatorControlFrame(testCase);
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                }
+            });
+            try {
                 lock.wait();
             } catch (InterruptedException ex) {
                 System.exit(2);
             }
-		}
-	}
+        }
+    }
 }
