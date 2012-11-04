8d812c71874fa8c6fa1a9d61143f01f50b5120ba
Benjamin Pylko <brpylko@gmail.com>
2012-11-04 19:58:10 +0000

    tried to fix ArrayWrappingProperty
    git-svn-id: https://svn.code.sf.net/p/frcsimulator/client/trunk@104 a72c890d-11d8-456d-a420-caba5f219908

--- net/sourceforge/frcsimulator/gui/propertyeditor/ArrayWrappingProperty.java
+++ net/sourceforge/frcsimulator/gui/propertyeditor/ArrayWrappingProperty.java
@@ -23,7 +23,8 @@ public class ArrayWrappingProperty<T> extends FrcBotSimProperty<T>{
 		// TODO figure out why get() != to when they're identical
 		//System.out.println(to.getClass() + ":"+get().getClass());
 		//System.out.println("Set "+to+" was "+get().equals(to) + " equal? "+(!get().equals(to)));
-		if (equals(to)) {
+		System.out.println(equals(to));
+		if (!equals(to)) {
 			Array.set(m_property.get(),m_index,to);
 			triggerChange();
 		}
@@ -34,14 +35,18 @@ public class ArrayWrappingProperty<T> extends FrcBotSimProperty<T>{
 	}
 
 	public boolean equals(Object check) {
+		//System.out.println(".equals CALLED!");
 		T value=get();
 		if (value.equals(check)) {
+			//System.out.println("true 1");
 			return true;
 		} else // It might still be equal, run some other checks
-		if (Number.class.isInstance(value)) {
-			System.out.println("Number check: "+((Double)((Number)check).doubleValue())+":"+(Double)(((Number)value).doubleValue()));
+		if (value instanceof Number) {
+			//System.out.println("Number check: "+((Double)((Number)check).doubleValue())+":"+(Double)(((Number)value).doubleValue()));
+			//System.out.println(((Double)((Number)check).doubleValue()).equals((Double)(((Number)value).doubleValue())));
 			return ((Double)((Number)check).doubleValue()).equals((Double)(((Number)value).doubleValue()));
 		} else {
+			//System.out.println("false!");
 			return false; // As far as we can tell, it's not equal
 		}
 	}
